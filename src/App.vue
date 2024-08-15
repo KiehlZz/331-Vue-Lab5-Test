3b496e8df0d0649c1065a5bbe48a8cf665300f77
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-08-16 00:04:31 +0700

    Before Deploy

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,83 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView, useRoute, useRouter } from 'vue-router'
+import { useMessageStore } from './stores/message';
+import { storeToRefs } from 'pinia';
+const store = useMessageStore()
+const { message } = storeToRefs(store)
+import { ref } from 'vue'
+
+const pageSizes = [1, 2, 4, 6, 8, 10]
+const pageSize = ref(pageSizes[1])
+const router = useRouter()
+const route = useRoute()
+
+const updatePageSize = () => {
+  router.push({ 
+    name: 'event-list-view',
+    query: { ...route.query, pageSize: pageSize.value, page: 1 }
+  })
+}
+
+if (route.query.pageSize) {
+  pageSize.value = parseInt(route.query.pageSize.toString())
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <SpeedInsights/>
+  <div class="text-center font-sans text-gray-700 antialias">
+    <header>
+      <div id="flashMessage" class="animate-fade" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
+      <h1>Deploy with Vercel</h1>
+      <div class="wrapper">
+        <nav class="py-6">
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view', query: { pageSize: pageSize } }">Event</RouterLink> |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink> |
+          <RouterLink :to="{ name: 'Student' }">Student</RouterLink> |
+        </nav>
+        <div>
+          <label class="mr-2" for="page-size">Event per page</label>
+          <select id="page-size" v-model="pageSize" @change="updatePageSize">
+            <option class="text-gray-700 hover:text-green-500"
+            exact-active-class="text-green-500" v-for="size in pageSizes" :key="size" :value="size">{{ size }}</option>
+          </select>
+        </div>
+      </div>
+    </header>
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style>
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  padding: 30px;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-nav a:first-of-type {
-  border: 0;
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+h2 {
+  font-size: 20px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
+@keyframes yellofade {
+  from {
+    background-color: yellow;
+  } to {
+    background-color: transparent;
   }
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#flashMessage {
+  animation: yellofade 3s ease-in-out;
 }
 </style>
